5942422ce169a90470216da15cd755024d0d3335
Dhi Aurrahman <dio@rockybars.com>
2024-11-07 16:35:52 +0700

    fix(supabase/migrations): Rename all ustad_ (#2)
    The previous migration files failed to rename all ustad_ to ustadz_.
    
    Signed-off-by: Dhi Aurrahman <dio@rockybars.com>

--- supabase/migrations/20241107080857_alter_ustad_id_to_ustadz_id.sql
+++ supabase/migrations/20241107080857_alter_ustad_id_to_ustadz_id.sql
@@ -6,8 +6,19 @@ ALTER TABLE "public"."halaqah" DROP COLUMN IF EXISTS "ustad_id";
 
 ALTER TABLE "public"."halaqah" ADD COLUMN IF NOT EXISTS "ustadz_id" character varying;
 
-ALTER TABLE "public"."halaqah" ADD CONSTRAINT "halaqah_ustadz_id_fkey"
-    FOREIGN KEY ("ustadz_id") REFERENCES "users"("email") NOT VALID;
+DO $$
+BEGIN
+    IF NOT EXISTS (
+        SELECT 1
+        FROM pg_constraint
+        WHERE conname = 'halaqah_ustadz_id_fkey'
+          AND conrelid = 'public.halaqah'::regclass
+    ) THEN
+        ALTER TABLE "public"."halaqah"
+        ADD CONSTRAINT "halaqah_ustadz_id_fkey"
+        FOREIGN KEY ("ustadz_id") REFERENCES "users"("email") NOT VALID;
+    END IF;
+END $$;
 
 ALTER TABLE "public"."halaqah" VALIDATE CONSTRAINT "halaqah_ustadz_id_fkey";
 
