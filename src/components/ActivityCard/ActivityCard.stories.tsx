1bf487baa64b73bc994a696326c5ede834ed79e1
Try Ajitiono <ballinst@gmail.com>
2024-11-13 22:43:56 +0700

    feat(ActivityCard): #22 implement activity card in ladle

---
+++ src/components/ActivityCard/ActivityCard.stories.tsx
@@ -0,0 +1,44 @@
+import { ActivityCard } from "./ActivityCard";
+
+export function ActivityCardStory() {
+  return (
+    <div className="grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-8">
+      <ActivityCard
+        id="1"
+        surahStart={{ name: 'At-Taubah', verse: "1" }}
+        surahEnd={{ name: 'At-Taubah', verse: "26" }}
+        timestamp={new Date().toISOString()}
+        pageAmount={4}
+        notes="Santri kesulitan menghafal ayat yang memiliki beberapa titik stop dan kemiripan di antara titik-titik tersebut."
+        type="Sabaq"
+      />
+      <ActivityCard
+        id="1"
+        surahStart={{ name: 'At-Taubah', verse: "1" }}
+        surahEnd={{ name: 'At-Taubah', verse: "1" }}
+        timestamp={new Date(2024, 1, 1, 7).toISOString()}
+        pageAmount={0}
+        notes="Santri tidak hadir karena sakit."
+        type="Sabaq"
+      />
+      <ActivityCard
+        id="1"
+        surahStart={{ name: 'Al-Baqarah', verse: "1" }}
+        surahEnd={{ name: 'Al-Baqarah', verse: "286" }}
+        timestamp={new Date(2024, 1, 1, 7).toISOString()}
+        pageAmount={49}
+        notes=""
+        type="Sabaq"
+      />
+      <ActivityCard
+        id="1"
+        surahStart={{ name: 'Al-Mumtahanah', verse: "1" }}
+        surahEnd={{ name: 'Al-Mumtahanah', verse: "13" }}
+        timestamp={new Date(2024, 1, 1, 7).toISOString()}
+        pageAmount={3}
+        notes=""
+        type="Sabaq"
+      />
+    </div>
+  )
+}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,5 @@
 import type { Config } from "tailwindcss";
+import plugin from "tailwindcss/plugin";
 
 const config: Config = {
   content: [
@@ -7,8 +8,47 @@ const config: Config = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        mtmh: {
+          primary: {
+            primary: '#602515'
+          },
+          secondary: {
+            secondary: '#F8AE1A'
+          },
+          neutral: {
+            10: '#F2F4F6',
+            20: '#D9DDE3',
+            50: '#8C8F93',
+            white: '#ffffff'
+          }
+        }
+      }
+    },
   },
-  plugins: [],
+  plugins: [
+    plugin(function ({ addUtilities }) {
+      addUtilities({
+        // TODO: not sure if there's a better way to do this?
+        '.text-mtmh-title-small': {
+          'font-size': '14px',
+          'font-weight': '500',
+          'line-height': '20px',
+          'text-align': 'left',
+          'text-underline-position': 'from-font',
+          'text-decoration-skip-ink': 'none',
+        },
+        '.text-mtmh-button-medium': {
+          'font-size': '14px',
+          'font-weight': '600',
+          'line-height': '20px',
+          'text-align': 'left',
+          'text-underline-position': 'from-font',
+          'text-decoration-skip-ink': 'none',
+        },
+      })
+    })
+  ],
 };
 export default config;
