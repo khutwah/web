a0ce81156a466f85828c3862d9090c39d5cdba31
faizrktm <faizkort@gmail.com>
2024-11-16 09:17:11 +0700

    fix: fix role falsy and reduce hop

--- src/utils/auth/page-access.ts
+++ src/utils/auth/page-access.ts
@@ -2,12 +2,16 @@ import "server-only";
 
 import { redirect } from "next/navigation";
 import { getUserRole } from "../supabase/get-user-role";
+import { PAGE_BY_ROLE } from "@/models/auth";
 
 export async function isHaveAccess(role: number) {
   const _role = await getUserRole();
 
-  if (!_role) return redirect("/");
-  if (_role !== role) return redirect("/");
+  // User has no access
+  if (_role === -1) return redirect("/");
+
+  // User not eligible to access expected page
+  if (_role !== role) return redirect(PAGE_BY_ROLE[_role] ?? "/");
 
   return true;
 }
