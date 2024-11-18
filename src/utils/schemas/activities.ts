b2f2b21ea5160ccf4fc0bf6f00e99ba00a919d00
Faiz Azmi Rekatama <faizkort@gmail.com>
2024-11-18 23:27:20 +0700

    feat: Create activity API (#45)
    * feat: create activity api
    
    * feat: create activity validation message
    
    * feat: adding halaqah name to activities result

--- src/utils/schemas/activities.ts
+++ src/utils/schemas/activities.ts
@@ -1,26 +1,43 @@
-import dayjs from "dayjs";
-import { string, number, object } from "yup";
+import { string, number, object, array, boolean } from "yup";
 
-const testTimestamp = (value?: string) => !value || dayjs(value).isValid();
-const transformToIsoString = (value: string) =>
-  value ? dayjs.unix(Number(value)).toISOString() : undefined;
+const testTimestamp = (value?: string) => !value || !isNaN(Date.parse(value));
 
 export const activityFilterSchema = object({
-  start_date: string()
-    .test(
-      "is-valid-date",
-      "Timestamp must represent a valid date",
-      testTimestamp
-    )
-    .transform(transformToIsoString),
-  end_date: string()
-    .test(
-      "is-valid-date",
-      "Timestamp must represent a valid date",
-      testTimestamp
-    )
-    .transform(transformToIsoString),
+  start_date: string().test(
+    "is-valid-date",
+    "Tanggal harus dalam format ISO yang valid",
+    testTimestamp
+  ),
+  end_date: string().test(
+    "is-valid-date",
+    "Tanggal harus dalam format ISO yang valid",
+    testTimestamp
+  ),
   type: number().oneOf([1, 2, 3] as const),
   limit: number().integer().min(1),
   offset: number().integer().min(0),
+});
+
+export const activityCreateSchema = object({
+  notes: string().required("Catatan wajib diisi"),
+  tags: array().of(string()).required("Tag wajib diisi"),
+  shift_id: number().required("ID shift wajib diisi"),
+  student_id: number().required("ID siswa wajib diisi"),
+  type: number()
+    .oneOf(
+      [1, 2, 3] as const,
+      "Tipe Aktivitas harus salah satu dari 1, 2, atau 3"
+    )
+    .required("Tipe Aktivitas wajib diisi"),
+  achieve_target: boolean().required("Target pencapaian wajib diisi"),
+  start_surah: number().required("Awal baca wajib diisi"),
+  end_surah: number().required("Akhir baca wajib diisi"),
+  start_verse: number().required("Awal ayat wajib diisi"),
+  end_verse: number().required("Akhir ayat wajib diisi"),
+  page_amount: number().required("Jumlah halaman wajib diisi"),
+  created_at: string().test(
+    "is-valid-date",
+    "Tanggal harus dalam format ISO yang valid",
+    testTimestamp
+  ),
 });
