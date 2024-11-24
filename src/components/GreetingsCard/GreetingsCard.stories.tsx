79ba51d452b7368cab6d5141788ce58bd9c707a1
Try Ajitiono <ballinst@gmail.com>
2024-11-24 10:57:26 +0700

    Add dates

--- src/components/GreetingsCard/GreetingsCard.stories.tsx
+++ src/components/GreetingsCard/GreetingsCard.stories.tsx
@@ -6,25 +6,33 @@ export function GreetingsCardStory() {
   const hours = [5, 9, 12, 16, 18, 20, 3]
 
   return (
-    <div className='grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-8'>
-      {hours.map((hour) => (
-        <GreetingsCard
-          key={hour}
-          avatarUrl={photo}
-          name='Ust. Fulan Abdurrahman'
-          salahPrayerTimes={{
-            imsak: '03:56',
-            subuh: '04:06',
-            terbit: '05:20',
-            dhuha: '05:48',
-            dzuhur: '11:37',
-            ashar: '14:52',
-            maghrib: '17:47',
-            isya: '18:59'
-          }}
-          currentDate={dayjs().hour(hour).minute(0).toDate()}
-        />
-      ))}
-    </div>
+    <ol className='grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-8'>
+      {hours.map((hour) => {
+        const currentDate = dayjs().hour(hour).minute(0).toDate()
+
+        return (
+          <li className='flex flex-col gap-y-2'>
+            <div>{currentDate.toLocaleString()}</div>
+
+            <GreetingsCard
+              key={hour}
+              avatarUrl={photo}
+              name='Ust. Fulan Abdurrahman'
+              salahPrayerTimes={{
+                imsak: '03:56',
+                subuh: '04:06',
+                terbit: '05:20',
+                dhuha: '05:48',
+                dzuhur: '11:37',
+                ashar: '14:52',
+                maghrib: '17:47',
+                isya: '18:59'
+              }}
+              currentDate={currentDate}
+            />
+          </li>
+        )
+      })}
+    </ol>
   )
 }
