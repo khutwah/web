7edec208bb1620cb641375580977897020a9a536
Try Ajitiono <ballinst@gmail.com>
2024-11-28 19:32:07 +0700

    yep

--- src/components/Badge/Badge.tsx
+++ src/components/Badge/Badge.tsx
@@ -2,25 +2,22 @@ import { cn } from '@/utils/classnames'
 
 interface BadgeProps {
   text: string
-  color: 'white' | 'green' | 'blue' | 'tamarind' | 'mute'
-  className?: string
+  color: 'green' | 'blue' | 'tamarind' | 'mute'
 }
 
 const COLOR_TO_CLASSNAME_RECORD: Record<BadgeProps['color'], string> = {
-  white: 'bg-mtmh-neutral-white text-mtmh-grey-base',
   blue: 'bg-mtmh-blue-base',
   green: 'bg-mtmh-green-base',
   tamarind: 'bg-mtmh-tamarind-base',
   mute: 'bg-mtmh-snow-base'
 }
 
-export function Badge({ color, text, className }: BadgeProps) {
+export function Badge({ color, text }: BadgeProps) {
   return (
     <div
       className={cn(
         'text-xs text-mtmh-neutral-white py-0.5 px-2 rounded-md',
-        COLOR_TO_CLASSNAME_RECORD[color],
-        className
+        COLOR_TO_CLASSNAME_RECORD[color]
       )}
     >
       {text}
