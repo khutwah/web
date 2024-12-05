f7382925a844b872ccf807dd8a24ad6bdb596e6d
Try Ajitiono <ballinst@gmail.com>
2024-12-05 19:54:37 +0700

    revert input with label

--- src/components/Form/InputWithLabel.tsx
+++ src/components/Form/InputWithLabel.tsx
@@ -1,35 +1,20 @@
 import { ComponentProps } from 'react'
 import { Input } from './Input'
 import { Label } from './Label'
-import { cn } from '@/utils/classnames'
 
 export function InputWithLabel({
   label,
-  inputProps,
-  error
+  inputProps
 }: {
   label: string
   inputProps: ComponentProps<typeof Input>
-  error?: string
 }) {
-  const { className, ...rest } = inputProps
-
   return (
     <div className='grid w-full items-center gap-2'>
-      <Label
-        htmlFor={inputProps.id}
-        className={cn('text-mtmh-title-small', error && 'text-mtmh-red-light')}
-      >
+      <Label htmlFor={inputProps.id} className='text-mtmh-title-small'>
         {label}
       </Label>
-      <Input
-        {...rest}
-        className={cn(className, error && 'border-mtmh-red-light')}
-      />
-
-      {error && (
-        <div className='text-mtmh-sm-regular text-mtmh-red-light'>{error}</div>
-      )}
+      <Input {...inputProps} />
     </div>
   )
 }
