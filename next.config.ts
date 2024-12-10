af07a78ac5d79c0e9fc1f73b8c8bd1e29041fb55
Dhi Aurrahman <dio@rockybars.com>
2024-12-10 17:37:59 +0700

    fix: Upgrade to canary to fix undici bug
    To pick up https://github.com/vercel/next.js/pull/73274.
    
    Signed-off-by: Dhi Aurrahman <dio@rockybars.com>

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
-  // https://nextjs.org/docs/advanced-features/output-file-tracing#automatically-copying-traced-files
+  // https://nextjs.org/docs/advanced-features/output-file-tracing#automatically-copying-traced-files.
   output: process.env.DOCKER_BUILD === 'true' ? 'standalone' : undefined,
   images: {
     remotePatterns: [
