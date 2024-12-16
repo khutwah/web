b42ddf25983780caac26d45d9e4ab30a86ba1a9c
Dhi Aurrahman <dio@rockybars.com>
2024-12-17 05:38:45 +0700

    chore: Run generateBuildId to uniquely define a deployment (#226)
    Signed-off-by: Dhi Aurrahman <dio@rockybars.com>

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,9 @@ import type { NextConfig } from 'next'
 const nextConfig: NextConfig = {
   // https://nextjs.org/docs/advanced-features/output-file-tracing#automatically-copying-traced-files.
   output: process.env.DOCKER_BUILD === 'true' ? 'standalone' : undefined,
+  generateBuildId() {
+    return process.env.NEXT_PUBLIC_APP_VERSION || 'development'
+  },
   images: {
     remotePatterns: [
       {
