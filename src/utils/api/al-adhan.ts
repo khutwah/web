2078ab4f3d6258aa90192b9ddbae0dbbe6601d7f
Dhi Aurrahman <dio@rockybars.com>
2024-12-18 13:38:48 +0700

    chore: Proxy mumtaz and aladhan to *.khutwah.id
    Signed-off-by: Dhi Aurrahman <dio@rockybars.com>

--- src/utils/api/al-adhan.ts
+++ src/utils/api/al-adhan.ts
@@ -8,7 +8,7 @@ export async function getAlAdhanPrayerTimings(
   position: { lat: number; long: number }
 ): Promise<AlAdhanPrayerTimingsResponse['data']> {
   // Reference: https://aladhan.com/prayer-times-api#tag/Daily-Prayer-Times/paths/~1v1~1timings~1%7Bdate%7D/get.
-  const url = new URL(`https://api.aladhan.com/v1/timings/${date}`)
+  const url = new URL(`https://aladhan.khutwah.id/v1/timings/${date}`)
   url.searchParams.set('latitude', `${position.lat}`)
   url.searchParams.set('longitude', `${position.long}`)
   // 20 is Kemenag's data source.
