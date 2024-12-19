f901f19d2020aedb58a8bea888cd61dc6d51c4a8
Dhi Aurrahman <dio-khutwah@rockybars.com>
2024-12-19 14:26:11 +0700

    chore: Simplify workflows
    Signed-off-by: Dhi Aurrahman <dio-khutwah@rockybars.com>

--- next.config.ts
+++ next.config.ts
@@ -7,6 +7,55 @@ const nextConfig: NextConfig = {
     return process.env.NEXT_PUBLIC_APP_VERSION || 'development'
   },
   generateEtags: true,
+  async headers() {
+    return [
+      {
+        source: '/login',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 's-maxage=31536000, maxage=86400, stale-while-revalidate=59'
+          }
+        ]
+      },
+      {
+        source: '/get-started',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'maxage=86400, stale-while-revalidate=59'
+          }
+        ]
+      },
+      {
+        source: '/ustadz',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'maxage=86400, stale-while-revalidate=59'
+          }
+        ]
+      },
+      {
+        source: '/santri',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'maxage=86400, stale-while-revalidate=59'
+          }
+        ]
+      },
+      {
+        source: '/',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 's-maxage=31536000'
+          }
+        ]
+      }
+    ]
+  },
   images: {
     remotePatterns: [
       {
