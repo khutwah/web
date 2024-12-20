d685a220c7da85ffd9dc14b7ec1f6894a7a817f3
faizrktm <faizkort@gmail.com>
2024-12-20 11:11:52 +0700

    feat: make the navbar sticky

--- src/app/ustadz/aktivitas/page.tsx
+++ src/app/ustadz/aktivitas/page.tsx
@@ -9,16 +9,20 @@ import { Skeleton } from '@/components/Skeleton/Skeleton'
 export default async function Aktivitas(props: Readonly<AktivitasPageProps>) {
   const params = await props.searchParams
   return (
-    <div>
-      <Navbar text='Aktivitas' />
-      <div className='p-4 pt-0 bg-mtmh-red-base'>
-        <Suspense fallback={<Skeleton className='h-10 w-full' />}>
-          <Filter />
+    <div className='relative overflow-auto'>
+      <header className='sticky top-0 bg-mtmh-red-base z-10'>
+        <Navbar text='Aktivitas' />
+        <div className='p-4 pt-0 bg-mtmh-red-base'>
+          <Suspense fallback={<Skeleton className='h-10 w-full' />}>
+            <Filter />
+          </Suspense>
+        </div>
+      </header>
+      <main>
+        <Suspense fallback={<ListSkeleton />}>
+          <ActivityList studentId={params.student_id} />
         </Suspense>
-      </div>
-      <Suspense fallback={<ListSkeleton />}>
-        <ActivityList studentId={params.student_id} />
-      </Suspense>
+      </main>
     </div>
   )
 }
