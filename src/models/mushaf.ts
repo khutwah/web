8ba1573231f81fb45e0f46955433fb1191370575
dio-khutwah <dio-khutwah@rockybars.com>
2024-12-23 21:02:06 +0700

    feat: Add getAyahLocationInfo (#258)
    This adds `mushaf.getAyahLocationInfo` to get the location info (related to parts, e.g. juz, page, distanceToEndJuz) of an ayah in a mushaf.
    
    Signed-off-by: Dhi Aurrahman <dio-khutwah@rockybars.com>

--- src/models/mushaf.ts
+++ src/models/mushaf.ts
@@ -2,3 +2,16 @@ export interface Page {
   page: number
   boundaries: { surah: number; ayah: number }[]
 }
+
+export interface AyahLocationInfo {
+  juz: number
+  page: number
+
+  // All of the following numbers are independent from each other.
+  // surah tells the surah difference, while ayah and page tells the ayah and page difference respectively.
+  distanceToJuzEnd: {
+    surah: number
+    ayah: number
+    page: number
+  }
+}
