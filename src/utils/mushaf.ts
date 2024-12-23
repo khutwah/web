87a781549757fd1e10f425581d04055fbfad508e
Dhi Aurrahman <dio-khutwah@rockybars.com>
2024-12-23 20:41:30 +0700

    feat: Add getAyahLocationInfo
    Signed-off-by: Dhi Aurrahman <dio-khutwah@rockybars.com>

--- src/utils/mushaf.ts
+++ src/utils/mushaf.ts
@@ -1,5 +1,7 @@
 import pages from '@/data/mushaf/pages.json'
-import { Page } from '@/models/mushaf'
+import { juz } from '@/data/mushaf/parts.json'
+import surahs from '@/data/mushaf/surahs.json'
+import { Page, AyahLocationInfo } from '@/models/mushaf'
 
 /**
  * Get the page object that contains the boundaries of the surah and ayah.
@@ -77,4 +79,69 @@ export function getEndSurahAndAyah(
     ayah: boundary.ayah,
     pageNumber: endPageNumber
   }
+}
+
+/**
+ * Get the location information of the surah and ayah.
+ * @param surah - The surah number.
+ * @param ayah - The ayah number.
+ * @returns The location information of the surah and ayah.
+ */
+export function getAyahLocationInfo(
+  surah: number,
+  ayah: number
+): AyahLocationInfo | undefined {
+  const page = getPage(surah, ayah)
+  if (!page) return undefined
+
+  // Find the juz that contains the page. By comparing page with juz start and end pages.
+  const found = juz.find(
+    (j) => j.start.page <= page.page && page.page <= j.end.page
+  )
+  if (!found) return undefined
+
+  const ayahCount = getAyahCount(surah, ayah, found.end.surah, found.end.ayah)
+  if (!ayahCount) return undefined
+
+  // Calculate the distance to the end of the juz.
+  const distanceToJuzEnd = {
+    surah: found.end.surah - surah,
+    ayah: ayahCount,
+    page: found.end.page - page.page
+  }
+
+  return {
+    juz: found.id,
+    page: page.page,
+    distanceToJuzEnd
+  }
+}
+
+/**
+ * Get the number of ayahs between the start and end surah and ayah.
+ * @param startSurah - The start surah number.
+ * @param startAyah - The start ayah number.
+ * @param endSurah - The end surah number.
+ * @param endAyah - The end ayah number.
+ * @returns The number of ayahs between the start and end surah and ayah.
+ */
+export function getAyahCount(
+  startSurah: number,
+  startAyah: number,
+  endSurah: number,
+  endAyah: number
+): number | undefined {
+  // If in the same surah, then we just need to consider the ayah distance in that surah.
+  if (startSurah === endSurah) {
+    return endAyah - startAyah
+  }
+
+  const startSurahInfo = surahs.find((surah) => surah.id === startSurah)
+  const startAyahToStartSurahEnd = startSurahInfo?.verses_count! - startAyah
+  let ayahDistance = 0
+  for (let next = startSurah + 1; next < endSurah; next++) {
+    const surahInfo = surahs.find((surah) => surah.id === next)
+    ayahDistance += surahInfo?.verses_count!
+  }
+  return startAyahToStartSurahEnd + endAyah + ayahDistance
 }
