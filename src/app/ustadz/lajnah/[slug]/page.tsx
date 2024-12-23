ede78eec786618abeff0f6290966de9d90ba2428
faizrktm <faizkort@gmail.com>
2024-12-23 13:51:01 +0700

    feat: wip lajnah detail page

---
+++ src/app/ustadz/lajnah/[slug]/page.tsx
@@ -0,0 +1,31 @@
+import { StateMessage } from '@/components/StateMessage/StateMessage'
+import { Lajnah } from '@/utils/supabase/models/lajnah'
+
+interface LajnahPageProps {
+  params: Promise<{ slug: number }>
+}
+
+export async function LajnahPage({
+  params: paramsPromise
+}: Readonly<LajnahPageProps>) {
+  const { slug } = await paramsPromise
+
+  const lajnahInstance = new Lajnah()
+  const lajnah = await lajnahInstance.list({
+    parent_lajnah_id: slug
+  })
+
+  if (lajnah.error) {
+    return (
+      <StateMessage
+        type='error'
+        title='Maaf, Ada Kesalahan di Sistem'
+        description='Terjadi kesalahan. Kami sedang memperbaiki. Mohon coba lagi nanti.'
+      />
+    )
+  }
+
+  console.log(lajnah)
+
+  return <div>{slug}</div>
+}
