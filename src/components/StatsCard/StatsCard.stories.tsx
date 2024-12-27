7aaf680fd5ee02deec7c1b58ee7e9ba0fc49d8b4
Dhi Aurrahman <dio-khutwah@rockybars.com>
2024-12-27 08:38:18 +0700

    Fix
    Signed-off-by: Dhi Aurrahman <dio-khutwah@rockybars.com>

--- src/components/StatsCard/StatsCard.stories.tsx
+++ src/components/StatsCard/StatsCard.stories.tsx
@@ -3,7 +3,7 @@ import { StatsCard } from './StatsCard'
 export function StatsCardStory() {
   return (
     <div className='grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-8'>
-      <StatsCard />
+      <StatsCard surah={2} ayah={1} halaqahCount={3} />
     </div>
   )
 }
