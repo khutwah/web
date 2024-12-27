9613953955f4fab1234e1ccff6b2acd19bfb56cd
dio-khutwah <dio-khutwah@rockybars.com>
2024-12-27 08:48:58 +0700

    feat: Query and show the real summary numbers (#266)
    Signed-off-by: Dhi Aurrahman <dio-khutwah@rockybars.com>

--- src/components/StatsCard/StatsCard.tsx
+++ src/components/StatsCard/StatsCard.tsx
@@ -1,20 +1,23 @@
 import { Card, CardContent } from '@/components/Card/Card'
 import { Tabs, TabsList, TabsTrigger } from '@/components/Tabs/Tabs'
 import { BookOpenText, AlignJustify, Clock } from 'lucide-react'
+import { getAyahLocationSummary } from '@/utils/mushaf'
 
-export function StatsCard() {
+interface StatsCardProps {
+  surah: number
+  ayah: number
+  halaqahCount: number
+}
+
+export function StatsCard({ surah, ayah, halaqahCount }: StatsCardProps) {
+  const summary = getAyahLocationSummary(surah, ayah)
+
   return (
     <div className='w-full'>
       <Tabs defaultValue='all-time' className='w-full'>
         <TabsList className='grid w-full grid-cols-3 mb-4'>
           <TabsTrigger value='all-time' className='text-sm'>
             ALL-TIME
-          </TabsTrigger>
-          <TabsTrigger value='2023' className='text-sm'>
-            2023
-          </TabsTrigger>
-          <TabsTrigger value='2022' className='text-sm'>
-            2022
           </TabsTrigger>
         </TabsList>
       </Tabs>
@@ -30,15 +33,26 @@ export function StatsCard() {
               <h3 className='text-mtmh-m-regular text-mtmh-grey-lightest mb-1'>
                 Juz
               </h3>
-              <div className='text-3xl font-bold'>20.50</div>
-              <div className='text-mtmh-grey-lightest'>68.33% perjalanan</div>
+              <div className='text-3xl font-bold'>
+                {summary?.juz.total.toFixed(2) || 0}
+              </div>
+              <div className='text-mtmh-grey-lightest'>
+                {summary?.juz.progress.toFixed(2) || 0}% perjalanan
+              </div>
             </div>
             <div>
               <h3 className='text-mtmh-m-regular text-mtmh-grey-lightest mb-1'>
                 Lajnah
               </h3>
-              <div className='text-3xl font-bold'>4x</div>
-              <div className='text-mtmh-grey-lightest'>20 juz sekali duduk</div>
+              <div className='text-3xl font-bold'>
+                {summary?.lajnah.previous && summary?.lajnah.previous > 0
+                  ? summary?.lajnah.previous / 5
+                  : 0}
+                x
+              </div>
+              <div className='text-mtmh-grey-lightest'>
+                {summary?.lajnah.previous || 0} juz sekali duduk
+              </div>
             </div>
           </div>
 
@@ -46,21 +60,21 @@ export function StatsCard() {
             <div className='flex items-center gap-2'>
               <BookOpenText className='w-4 h-4 text-mtmh-tamarind-dark' />
               <div>
-                <div className='font-medium'>300</div>
+                <div className='font-medium'>{summary?.pages || 0}</div>
                 <div className='text-xs text-mtmh-grey-lightest'>Halaman</div>
               </div>
             </div>
             <div className='flex items-center gap-2'>
               <AlignJustify className='w-4 h-4 text-mtmh-tamarind-dark' />
               <div>
-                <div className='font-medium'>4000</div>
+                <div className='font-medium'>{summary?.ayahs || 0}</div>
                 <div className='text-xs text-mtmh-grey-lightest'>Ayat</div>
               </div>
             </div>
             <div className='flex items-center gap-2'>
               <Clock className='w-4 h-4 text-mtmh-tamarind-dark' />
               <div>
-                <div className='font-medium'>300x</div>
+                <div className='font-medium'>{halaqahCount || 0}x</div>
                 <div className='text-xs text-mtmh-grey-lightest'>Halaqah</div>
               </div>
             </div>
--- src/models/mushaf.ts
+++ src/models/mushaf.ts
@@ -5,11 +5,21 @@ export interface Page {
 
 export interface AyahLocationInfo {
   juz: number
+  totalJuz: number
   page: number
+  lajnah: {
+    id: number
+    range: number
+  }
 
   // All of the following numbers are independent from each other.
   // surah tells the surah difference, while ayah and page tells the ayah and page difference respectively.
   distanceToJuzEnd: {
+    surah: number
+    ayah: number
+    page: number
+  }
+  distanceFromJuzStart: {
     surah: number
     ayah: number
     page: number
