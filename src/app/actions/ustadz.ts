8a99a38c404bf7c3777faa3499cc8f6290556fda
Kukuh Sulistyo <sulistyo.kukuh@gmail.com>
2024-12-27 08:55:38 +0700

    fix: update ustadz action

--- src/app/actions/ustadz.ts
+++ src/app/actions/ustadz.ts
@@ -7,18 +7,21 @@ import {
   createSuccessResponse
 } from '@/utils/api/response-generator'
 import { User } from '@/utils/supabase/models/user'
+import { ROLE } from '@/models/auth'
 
 export async function getUstadzs() {
   try {
     const userInstance = new User()
 
-    const result = await userInstance.list({ role: 2 })
+    const result = await userInstance.list({ role: ROLE.USTADZ })
 
     if (result.error) {
       throw result.error
     }
 
-    return createSuccessResponse({ data: result.data })
+    return createSuccessResponse({
+      data: result.data?.map((item) => ({ id: item.id, name: item.name }))
+    })
   } catch (error: unknown) {
     const e = error as PostgrestError
 
