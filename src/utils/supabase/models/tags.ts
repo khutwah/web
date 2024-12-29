185a8cff964a1bc9db44478bc44584a35061fcbf
Dhi Aurrahman <dio-khutwah@rockybars.com>
2024-12-29 08:40:49 +0700

    fix: getTags
    Signed-off-by: Dhi Aurrahman <dio-khutwah@rockybars.com>

--- src/utils/supabase/models/tags.ts
+++ src/utils/supabase/models/tags.ts
@@ -2,6 +2,6 @@ import { Base } from './base'
 
 export class Tags extends Base {
   async list() {
-    return await (await this.supabase).from('tags').select(`name, type`)
+    return (await this.supabase).from('tags').select(`name, category`)
   }
 }
