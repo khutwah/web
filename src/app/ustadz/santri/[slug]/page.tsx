52c4643028195b8977906c98e6e47d115d0de5a8
Dhi Aurrahman <dio-khutwah@rockybars.com>
2025-01-09 19:21:19 +0700

    Fix
    Signed-off-by: Dhi Aurrahman <dio-khutwah@rockybars.com>

--- src/app/ustadz/santri/[slug]/page.tsx
+++ src/app/ustadz/santri/[slug]/page.tsx
@@ -3,12 +3,9 @@ import { ROLE } from '@/models/auth'
 import UstadzRole from './components/roles/UstadzRole'
 import LajnahRole from './components/roles/LajnahRole'
 import { DetailSantriProps } from '../models/detail-santri'
-import { Refresher } from '@/components/Page/Refresher'
 
 export default async function DetailSantri(props: DetailSantriProps) {
   const role = await getUserRole()
-  const id = (await props.params).slug
-
   return (
     <>
       {role === ROLE.USTADZ ? (
