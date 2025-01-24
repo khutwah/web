d26a874d6a32e69a9c9895e78f8e0f69ef80c3a3
Dhi Aurrahman <dio-khutwah@rockybars.com>
2025-01-24 18:54:10 +0700

    fix: call_draft_activities_all to check weekend in GMT+7
    Signed-off-by: Dhi Aurrahman <dio-khutwah@rockybars.com>

--- supabase/migrations/20250124074508_schedule_draft_activities_all.sql
+++ supabase/migrations/20250124074508_schedule_draft_activities_all.sql
@@ -1,6 +1,13 @@
 CREATE
-OR REPLACE FUNCTION call_draft_activities_all () RETURNS void AS $$
+OR REPLACE FUNCTION call_draft_activities_all () RETURNS VOID AS $$
 BEGIN
+    -- Check if the current day is a weekend (Saturday or Sunday) in GMT+7
+    IF EXTRACT(ISODOW FROM (CURRENT_TIMESTAMP AT TIME ZONE 'GMT+7')) IN (6, 7) THEN
+        RAISE NOTICE 'Skipping execution: Today is a weekend in GMT+7.';
+        RETURN; -- Silently exit the function
+    END IF;
+
+    -- Perform the draft activities if not a weekend
     PERFORM draft_activities(1::SMALLINT);
     PERFORM draft_activities(2::SMALLINT);
     PERFORM draft_activities(3::SMALLINT);
